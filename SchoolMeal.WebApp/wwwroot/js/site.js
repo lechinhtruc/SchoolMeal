eb9fab019728582500ab7d05bd101d2dd21dfb30
letruc <lechinhtruc999@gmail.com>
2023-09-24 03:47:20 +0700

    ✨✨

--- SchoolMeal.WebApp/wwwroot/js/site.js
+++ SchoolMeal.WebApp/wwwroot/js/site.js
@@ -1,7 +1,11 @@
 ﻿$(document).ready(() => {
-    $('.deleteUserBtn').on('click', (e) => {
+    $('.delete-user-btn').on('click', (e) => {
         const userId = $(e.currentTarget).attr('delete-user-id');
         $('#userId').val(userId);
         $('#confirmModal').modal("show");
-    })
+    }); //Open delete user confirm modal
+
+    $('.edit-user-btn').on('click', (e) => {
+        //write code here.... //..
+    }) //Open edit user modal
 })
